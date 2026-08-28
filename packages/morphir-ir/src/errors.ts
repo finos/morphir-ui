08a84d0a6df85a69210fe32fdea3df6c4ec3e226
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2026-08-27 21:36:50 -0500

    feat(ir): decode morphir-ir.json v3 envelope with friendly version errors
    Signed-off-by: Damian Reeves <957246+DamianReeves@users.noreply.github.com>

---
+++ packages/morphir-ir/src/errors.ts
@@ -0,0 +1,31 @@
+import { Data } from 'effect'
+
+export class InvalidJson extends Data.TaggedError('InvalidJson')<{ readonly message: string }> {}
+
+export class MissingFormatVersion extends Data.TaggedError('MissingFormatVersion')<{
+  readonly message: string
+}> {
+  static make = () =>
+    new MissingFormatVersion({
+      message:
+        "The IR is in an old format that doesn't have a format version on it. Please regenerate it!",
+    })
+}
+
+export class UnsupportedFormatVersion extends Data.TaggedError('UnsupportedFormatVersion')<{
+  readonly found: number
+  readonly message: string
+}> {
+  static make = (found: number) =>
+    new UnsupportedFormatVersion({
+      found,
+      message:
+        found === 1
+          ? 'The IR is using format version 1, a legacy format that morphir-ui does not support yet. Please regenerate it with a current morphir-elm!'
+          : `The IR is using format version ${found} but the latest format version is 3. Please regenerate it!`,
+    })
+}
+
+export class InvalidIr extends Data.TaggedError('InvalidIr')<{ readonly message: string }> {}
+
+export type IrError = InvalidJson | MissingFormatVersion | UnsupportedFormatVersion | InvalidIr
---
+++ packages/morphir-ir/src/index.ts
@@ -0,0 +1,2 @@
+export * from './decode.ts'
+export * from './errors.ts'
